bacd75d92cd274cae699520d3d3c47d7c4258cb7
Natalia Dzioba <natalia.dzioba@gmail.com>
2016-11-10 22:46:31 +0200

    Initial Commit

---
+++ n_ix_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  n_ix_test
+//
+//  Created by Natalia Dzioba on 11/10/16.
+//  Copyright © 2016 Natalia Dzioba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ n_ix_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  n_ix_test
+//
+//  Created by Natalia Dzioba on 11/10/16.
+//  Copyright © 2016 Natalia Dzioba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
